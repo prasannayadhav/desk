b7345c482bb0e09aca72cca2ccb3ba6eba3e1578
prasannayadhav <durgavaraprasanna999@gmail.com>
2023-11-15 11:00:07 +0530

    commit3

---
+++ add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    printf("enter the values a,b,c");
+    scanf("%d,%d,%d",&a,&b,&c);
+    d = ((a+b)+(a-b));
+    printf("enter the value of d is %d,d");
+    return 0;
+}
---
+++ area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    float l, b, area;
+    printf("enter length of rectangle:");
+    scanf("%f", &l);
+    printf("enter breadth of rectangle:");
+    scanf("%f", &b);
+    area = (l * b);
+    printf("area of rectangle: %f\n", area);
+}
