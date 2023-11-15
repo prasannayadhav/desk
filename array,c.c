11bcb9c69b22bf27488829944f5319bded013295
prasannayadhav <durgavaraprasanna999@gmail.com>
2023-11-15 11:06:44 +0530

    commit

---
+++ array,c.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int  mynum[]={20,40,60,80,100};
+
+    printf("the output will be: %d", mynum);
+    return 0;
+}
--- operator.c
+++ operator.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int a,b;
-    a=24;
-    b=11;
-    printf("a+b =%d\n", a+b);
-    printf("a-b =%d\n", a-b);
-    printf("a*b =%d\n", a*b);
-    printf("a/b =%d\n", a/b);
+    int a, b;
+    a = 24;
+    b = 11;
+    printf("a+b =%d\n", a + b);
+    printf("a-b =%d\n", a - b);
+    printf("a*b =%d\n", a * b);
+    printf("a/b =%d\n", a / b);
     return 0;
 }
