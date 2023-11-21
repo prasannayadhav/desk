f398feb7061ff3a89272586ec9ee9d4d7c03b012
prasannayadhav <durgavaraprasanna999@gmail.com>
2023-11-21 11:06:44 +0530

    testconfirm

--- arr.c
+++ arr.c
@@ -2,12 +2,10 @@
 int main()
 {
     int arr[5] = {10, 15, 20, 25, 30, 35, 40};
-
     printf("element of arr[5]:%d\n", arr[5]);
     printf("element of arr[2]:%d\n", arr[2]);
     printf("element of arr[1]:%d\n", arr[1]);
     printf("element of arr[3]:%d\n", arr[3]);
     printf("element of arr[4]:%d", arr[4]);
-    
     return 0;
 }
